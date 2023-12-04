aef21aeba6ddf715e8ac902a499bc70a6f22175d
tunguyen <tunguyenhv@gmail.com>
2023-12-04 22:51:32 +0700

    change vnp config

--- src/config/vnp.config.ts
+++ src/config/vnp.config.ts
@@ -1,6 +1,6 @@
 export const vnpConfig = {
-  vnp_TmnCode: "WVAVN7JM",
-  vnp_HashSecret: "YMUNAHNFCJKOELDLPWRQLLNUOMQZEQXR",
+  vnp_TmnCode: "UUE2WAA9",
+  vnp_HashSecret: "IUICLNGZMZTEYDZDITBZJKFVXKRZXPWF",
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
   vnp_ReturnUrl: "https://learn-together.onrender.com/api/v1/vnp_return",
