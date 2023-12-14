d61798b98c74778e869d1e8a13639fb70e10e201
tunguyen <tunguyenhv@gmail.com>
2023-12-14 16:01:39 +0700

    .

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -70,14 +70,14 @@ export const accessTokenCookieOptions: CookieOptions = {
   maxAge: 86400000, // 24 hours
   httpOnly: true,
   // domain: "learn-together-app.azurewebsites.net", // Đặt tên miền của server ở đây
-  // domain: "learn-together.onrender.com", // Đặt tên miền của server ở đây
-  // path: "/",
-  // sameSite: "lax", // Hoặc "strict" tùy theo yêu cầu
-  // secure: true, // Nếu bạn sử dụng HTTPS, hãy đặt true, nếu không, đặt false
-  domain: "localhost",
+  domain: "learn-together.onrender.com", // Đặt tên miền của server ở đây
   path: "/",
-  sameSite: "lax",
-  secure: false,
+  sameSite: "lax", // Hoặc "strict" tùy theo yêu cầu
+  secure: true, // Nếu bạn sử dụng HTTPS, hãy đặt true, nếu không, đặt false
+  // domain: "localhost",
+  // path: "/",
+  // sameSite: "lax",
+  // secure: false,
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {
